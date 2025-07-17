ae9a8dca4156c0cf4e600b51573a5fe45b3a8359
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-07-18 00:49:42 +0800

    define node names in consts.py

---
+++ graph/consts.py
@@ -0,0 +1,11 @@
+# define constants, aka the node names that we're
+# going to refer to in the graph
+# the reason we do this to avoid code duplication
+# so everytime we reference the nodes we'll
+# actually reference the const, and if we want to
+# change the name for some reason we'll only need
+# to change it in one place
+RETRIEVE = "retrieve"
+GRADE_DOCUMENTS = "grade_documents"
+GENERATE = "generate"
+WEBSEARCH = "web_search"
